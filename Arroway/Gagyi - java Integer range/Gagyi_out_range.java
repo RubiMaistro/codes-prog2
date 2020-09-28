f4c047d987029c4ef96e16f28c33b98e6a0687ec
RubiMaistro <55796855+RubiMaistro@users.noreply.github.com>
2020-09-28 14:37:46 +0200

    Gagyi

---
+++ Arroway/Gagyi - java Integer range/Gagyi_out_range.java
@@ -0,0 +1,23 @@
+package Feladatok;
+
+public class Gagyi_out_range {
+
+	public static void main(String[] args) {
+		
+		Integer x = -129;
+		Integer t = -129;
+		
+		/*.out.println (x);
+		System.out.println (t);
+		*/
+		int a = 0;
+		
+		do {
+			a = a + 1;
+			System.out.println(a);
+		} while(x <= t && x >= t && t != x);
+
+
+	}
+
+}
