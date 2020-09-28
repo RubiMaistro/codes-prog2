44bc5d56519046d4d13a74bb99dd273a48d7b194
RubiMaistro <55796855+RubiMaistro@users.noreply.github.com>
2020-09-28 14:35:40 +0200

    Gagyi

---
+++ Gagyi.java
@@ -0,0 +1,23 @@
+package Feladatok;
+
+public class Gagyi {
+
+	public static void main(String[] args) {
+		
+		Integer x = -128;
+		Integer t = -128;
+		
+		/*System.out.println (x);
+		System.out.println (t);
+		*/
+		int a = 0;
+		
+		do {
+			a = a + 1;
+			System.out.println(a);
+		} while(x <= t && x >= t && t != x);
+
+	}
+
+}
+
